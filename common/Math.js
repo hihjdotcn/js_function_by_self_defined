70e33e383788e4e907dce08cc80b9d74fe450872
hihjdotcn <35172552+hihjdotcn@users.noreply.github.com>
2019-12-30 10:36:13 +0800

    更新随机数方法

--- common/Math.js
+++ common/Math.js
@@ -10,3 +10,15 @@ console.log(Math.pow(2, 3))
 console.log(Math.sqrt(2))
 // 返回数的绝对值
 console.log(Math.abs(-1.39))
+// 向下取整
+console.log(Math.floor(4.6))
+// 四舍五入
+console.log(Math.round(4.1))
+// 向上取整
+console.log(Math.ceil(4.1))
+// 获取范围内的随机数
+function randomNumber(min, max) {
+  let target = min + Math.round(Math.random() * (max - min))
+  return target
+}
+console.log(randomNumber(2, 9))
